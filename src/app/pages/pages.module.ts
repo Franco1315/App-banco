c8a7805798ea634353279712edd3bd27231c8360
Maria_franco <chiqui0205@gmail.com>
2023-06-14 12:09:49 +0200

    implementado login

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { LoginComponent } from '../auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    FormsModule
   ]
 })
 export class PagesModule { }
